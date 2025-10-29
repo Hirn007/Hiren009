800ab31d2b0b2ca151380fa825cddf96b6c7a1ac
Hirn007/php_2025 <hirenchunara1999@gmail.com>
2025-10-29 13:25:18 +0530

    first commit

---
+++ project/admin_panel/admin/icon-tabler.php
@@ -0,0 +1,26 @@
+<?php
+include_once('header.php');
+?>
+      <div class="body-wrapper-inner">
+        <div class="container-fluid">
+          <div class="card w-100 h-100 position-relative overflow-hidden">
+            <div class="card-body">
+              <h5 class="card-title fw-semibold mb-4">Icons</h5>
+              <iframe src="https://tabler-icons.io/" frameborder="0" style="height: calc(100vh - 250px); width: 100%;"
+                data-simplebar=""></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="./assets/libs/jquery/dist/jquery.min.js"></script>
+  <script src="./assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="./assets/js/sidebarmenu.js"></script>
+  <script src="./assets/js/app.min.js"></script>
+  <script src="./assets/libs/simplebar/dist/simplebar.js"></script>
+  <!-- solar icons -->
+  <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+</body>
+
+</html>
